b131775a63ac96aa8886b061a6e1316024b1a873
Rabitttt <burakkaragol60@gmail.com>
2022-09-25 23:57:34 +0000

    first commit

---
+++ src/test/java/com/example/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
